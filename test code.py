d9c0b8d8014774a0e73c363c9cde59b197036491
TBuscemi <thomasbuscemi124@gmail.com>
2021-07-16 18:57:46 -0400

    Did some work on the comparing p1 to p2 and got rock all setup

--- test code.py
+++ test code.py
@@ -15,22 +15,43 @@
 #     if(input_1 == "rock" and input_2 == "scissors"):
 
 # MIGHT MAKE THE INPUT AN NUMBER TO IT WILL MATCH UP WITH A LIST
-player_input = ""
-def player_move():
-    player_input = input("Please choose an option?\nROCK\nPAPER\nSCISSORS\nSPOCK\nLIZARD")
-    if(player_input.lower == "rock"):
-        player_input = "rock"
-    elif(player_input.lower == "paper"):
-        player_input = "paper"
-    elif(player_input.lower == "scissors"):
-        player_input =  "scissors" 
-    elif(player_input.lower == "spock"):
-        player_input = "spock"
-    elif(player_input.lower == "lizard"):
-        player_input = "lizard"     
-    else:
-        print("Hmmmm looks woops try again!")
-        player_move()
+# player_input = ""
+# def player_move():
+#     player_input = input("Please choose an option?\nROCK\nPAPER\nSCISSORS\nSPOCK\nLIZARD")
+#     if(player_input.lower == "rock"):
+#         player_input = "rock"
+#     elif(player_input.lower == "paper"):
+#         player_input = "paper"
+#     elif(player_input.lower == "scissors"):
+#         player_input =  "scissors" 
+#     elif(player_input.lower == "spock"):
+#         player_input = "spock"
+#     elif(player_input.lower == "lizard"):
+#         player_input = "lizard"     
+#     else:
+#         print("Hmmmm looks woops try again!")
+#         player_move()
+
+p1 = 0
+p2 = 0
+user_1 = ""
+user_2= ""
 
 
-
+def who_win():
+    if(user_1 == user_2):
+        print("no one wins")
+    if(user_1 == "rock"):
+        elif(user_2 == "scissors"):
+            print("rock smashes scissors you win #1!")
+            return p1 + 1    
+        elif(user_2 == "paper"):
+            print("paper covers rock you win #2")
+            return p2 + 1
+        elif(user_2 == "spock"):
+            print("spock vaporizes rock you win #2!")
+            return p2 + 1
+        elif(user_2 == "lizard"):
+            print("rock crushes lizard you win #1!")
+            return p1 + 1 
+    elif(user_1 == "papper"):
