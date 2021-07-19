75cf2d35901bec97bd18629d85781128c33da9c1
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-07-18 23:27:27 -0500

    added pseudo validation for player class

--- v2/human.py
+++ v2/human.py
@@ -7,9 +7,10 @@ class Human(Player):
         self.human_score = 0
         super().__init__()
 
-    #Use 1-5 input to select which gesture the player is using. Not sure if I want to keep numbers or change the numbers to the actual names
+    # Use 1-5 input to select which gesture the player is using. Not sure if I want to keep numbers or change the numbers to the actual names
     def human_move(self):
-        user_input = input("Make your move! \nRock: '1' \nPaper: '2' \nScissors: '3' \nLizard: '4' \nSpock: '5' \nPlease pick a number 1-5: ")
+        user_input = input(
+            "Throw down! \nRock: '1' \nPaper: '2' \nScissors: '3' \nLizard: '4' \nSpock: '5' \nPlease pick a number 1-5: ")
         if user_input == '1':
             user_input = "Rock"
         if user_input == '2':
@@ -21,6 +22,8 @@ class Human(Player):
         if user_input == '5':
             user_input = "Spock"
         else:
-           print("Try again")
-           user_input = False
+            # this should validate the input for the human/player gesture
+            print(
+                "Enter a valid option: \nRock: '1' \nPaper: '2' \nScissors: '3' \nLizard: '4' \nSpock: '5'")
+            user_input = False
         return user_input
